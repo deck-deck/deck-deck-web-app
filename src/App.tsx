8542686c6919a0271facb31c1b66d5a4db5146cf
Ignacio Cuadra <ignacio.cuadra.z@gmail.com>
2024-03-25 19:21:35 -0300

    feat: update verbs to add templates

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ function App() {
       <MainTemplate>
         <Card size="md">
           <Card.Front>
-            <h1 class="block font-bold text-2xl text-center">Front</h1>
+            <span class="font-bold text-4xl">spanish</span>
           </Card.Front>
           <Card.Back>
-            <h1 class="font-bold text-2xl text-center">Back</h1>
+            <dl class="flex flex-col items-center">
+              <dt class="font-mono text-sm">present</dt>
+              <dd class="mb-5 font-bold text-xl">present</dd>
+              <dt class="font-mono text-sm">simple past</dt>
+              <dd class="mb-5 font-bold text-xl">simplePast</dd>
+              <dt class="font-mono text-sm">past participle</dt>
+              <dd class="font-bold text-xl">pastParticiple</dd>
+            </dl>
           </Card.Back>
         </Card>
       </MainTemplate>
